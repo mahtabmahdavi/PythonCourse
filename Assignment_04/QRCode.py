2d24b8c16c7fbff9a0c4ebf51393199de784d990
Mahtab Mahdavi <91895285+mahtabmahdavi@users.noreply.github.com>
2022-11-27 19:23:25 +0330

    Delete Assignment_04 directory

--- Assignment_04/QRCode.py
+++
@@ -1,8 +0,0 @@
-import qrcode
-
-first_name = input("First Name: ")
-last_name = input("Last Name: ")
-phone = input("Phone: ")
-
-image = qrcode.make(f"Name: {first_name} {last_name}\nPhone: {phone}")
-image.save("ContactInformation.jpg")
